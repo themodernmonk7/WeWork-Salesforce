c7fdaefbcec350f0e6d4fe9e692ee9c02944e708
themodernmonk7 <sauravavishek@gmail.com>
2023-05-10 12:47:56 +0530

    import and export components

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -1,5 +1,9 @@
-import Btn from "./Buttons/Btn"
+import Btn from "./Btn"
+import CompanyProfile from "./CompanyProfile"
 import Header from "./Header"
+import Logo from "./Logo"
 import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import SidebarNavigation from "./SidebarNavigation"
 
-export { Navbar, Header, Btn }
+export { Navbar, Header, Btn, Sidebar, CompanyProfile, Logo, SidebarNavigation }
