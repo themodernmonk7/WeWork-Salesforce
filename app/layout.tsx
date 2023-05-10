fc7b4ec91d902f978666e10f0b341514b4071e2b
themodernmonk7 <sauravavishek@gmail.com>
2023-05-10 12:49:10 +0530

    add: fonts and body color

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={montserrat.className}>{children}</body>
+      <body className={`montserrat.className bg-[#F2FBF9] `}>{children}</body>
     </html>
   )
 }
