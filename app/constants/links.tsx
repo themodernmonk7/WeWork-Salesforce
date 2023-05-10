ab40f91ba24af838b0c2b8a88feeeb406d0886ab
themodernmonk7 <sauravavishek@gmail.com>
2023-05-10 12:48:31 +0530

    centralized links

---
+++ app/constants/links.tsx
@@ -0,0 +1,30 @@
+import {
+  SearchIcon,
+  InboxIcon,
+  BellIcon,
+  QuestionMarkCircleIcon,
+} from "./icons"
+
+export const links = [
+  {
+    id: 1,
+    text: "Search",
+    icon: <SearchIcon />,
+  },
+
+  {
+    id: 2,
+    text: "Inbox",
+    icon: <InboxIcon />,
+  },
+  {
+    id: 3,
+    text: "Notifications",
+    icon: <BellIcon />,
+  },
+  {
+    id: 4,
+    text: "Help",
+    icon: <QuestionMarkCircleIcon />,
+  },
+]
