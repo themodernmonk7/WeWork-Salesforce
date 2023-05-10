5a9bb6d5ccdf08950cc8ca80fc93a6c1c31341c7
themodernmonk7 <sauravavishek@gmail.com>
2023-05-10 12:45:49 +0530

    add: mobile responsive

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from "react"
 const Sidebar = () => {
   return (
     <>
-      <aside className=" bg-white w-56 px-5 py-4 rounded-xl shadow-sm  ">
+      <aside className=" hidden lg:block bg-white xl:w-2/12 px-5 py-4 rounded-xl shadow-sm h-full   ">
         <p className=" text-text-color-200 font-medium text-[18px] mb-4 ">
           Overview
         </p>
